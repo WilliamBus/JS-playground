ab44729b48e77374053671c7b86ba6fd7a825ca0
William Busbridge <william-busbridge@live.nmit.ac.nz>
2024-03-07 14:58:38 +1300

    if else learning

--- number.js
+++ number.js
@@ -1,14 +1,55 @@
-//NOTE - assigns a chosen variable (num1) to a chosen value (10) and cannot be changed
-const num1 = 10
-console.log (num1)
-//NOTE - assigns a chosen variable (num2) to a chosen value (20) and the value can be changed
-let num2 = 20
-console.log (num2)
-//NOTE - assigns a chosen variable (num3) to a chosen value (30) and can be changed (not used often anymore)
-var num3 = 30
-console.log (num3)
-/*
-"learnt" about const, let and var and that var isnt used anymore because it is an old version of const and let combined
-:)
-(already knew about this stuff)
-*/
+// /*
+// "learnt" about const, let and var and that var isnt used anymore because it is an old version of const and let combined
+// :)
+// (already knew about this stuff)
+// */
+
+// //NOTE - assigns a chosen variable (num1) to a chosen value (10) and cannot be changed
+// const pi = 3.14
+// console.log (pi)
+
+// //NOTE - assigns a chosen variable (num2) to a chosen value (20) and the value can be changed
+// let num2 = 20
+// num2 = 34
+// console.log (num2)
+
+// //NOTE - assigns a chosen variable (num3) to a chosen value (30) and can be changed (not used often anymore)
+// var num3 = 30
+// console.log (num3)
+
+// const fullName = "William Busbridge"
+// console.log(fullName)
+
+// const studyplace = "NMIT"
+// console.log(studyplace)
+
+
+// //NOTE - concatenation = + " " +
+// console.log(fullName + " " + studyplace)
+
+// console.log(`My full name is ${fullName} and I study at ${studyplace}`)
+
+// const working = true
+// const notWorking = false
+
+// if (working) {
+//     console.log(fullName)
+// } else {
+//     console.log("Die")
+// }
+
+// if (pi === 3.14) {
+//     console.log(fullName)
+// } else {
+//     console.log("Die")
+//}
+
+let a = 2
+let result
+// if() {} else {}
+if (a > 0) {
+    result = "positive"
+} else {
+    result = "Not Positive"
+}
+console.log (result)
