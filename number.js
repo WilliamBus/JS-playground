3872b4702101b2c3dfb2bf07cb5a222ceccd7fd3
William Busbridge <william-busbridge@live.nmit.ac.nz>
2024-03-07 13:48:35 +1300

    assiging variables to values using const,let and var

---
+++ number.js
@@ -0,0 +1,12 @@
+//NOTE - assigns a chosen variable (num1) to a chosen value (10) and cannot be changed
+const num1 = 10
+console.log (num1)
+//NOTE - assigns a chosen variable (num2) to a chosen value (20) and the value can be changed
+let num2 = 20
+console.log (num2)
+//NOTE - assigns a chosen variable (num3) to a chosen value (30) and can be changed (not used often anymore)
+var num3 = 30
+console.log (num3)
+/*
+
+*/
