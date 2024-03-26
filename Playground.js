2b88ae60a67909595a8d5b4635e8a2fef5b35177
William Busbridge <william-busbridge@live.nmit.ac.nz>
2024-03-26 14:38:01 +1300

    date

--- Playground.js
+++ Playground.js
@@ -396,7 +396,6 @@
 // const countDownArray = num => Array.from(Array(num + 1).keys()).reverse()
 // console.log(countDownArray(10))
 
-// //SECTION - Math
 // //TODO - Create a function that takes two numbers and a math operator (+, -, /, *) and will perform a calculation with the given numbers.
 // //NOTE - ex: cal (2, "+", 3) -> 5
 // function calNum(num1, operator, num2) {
@@ -412,16 +411,39 @@
 // const numCal = (num1, operator, num2) => operator + num2 === "/0" ? "Can not divide by 0!" : eval(num1+operator+num2)
 // console.log(numCal(9, "/", 0));
 
-//TODO - rewrite the ES5 with switch instead of if
-function calNum2(num1, operator, num2) {
-    switch (operator) {
-        case '+': return num1+num2;
-        case '-': return num1-num2;
-        case '*': return num1*num2;
-        case '/': return num1/num2;
-        if (operator = '/' && num2 === 0) {
-            "Can not divide by 0!"
-        }
-    }
+// //TODO - rewrite the ES5 with switch instead of if
+// function calNum2(num1, operator, num2) {
+//     switch (operator) {
+//         case '+': return num1+num2;
+//         case '-': return num1-num2;
+//         case '*': return num1*num2;
+//         case '/': return num1/num2;
+//         if (operator = '/' && num2 === 0) {
+//             "Can not divide by 0!"
+//         }
+//     }
+// }
+// console.log(calNum(9, "/", 0));
+
+// //TODO - Create an array that rotates the values clockwise by one.
+// //NOTE - The last value should then be the first.
+// //NOTE - ex. rotateArray([20, 15, 26, 22, 30]) -> [30, 20, 15, 26, 22]
+// function rotateArray(arr) {
+//     let lastValue = arr.pop()
+//     arr.unshift(lastValue)
+//     return arr
+// }
+
+// console.log(rotateArray([20, 15, 26, 22, 30]))
+
+//TODO - Create a function that takes a given date ("3/26/2024") return the day of the date as a string ("Tuesday") assumingthe week starts on Sunday.
+function getDayName(day) {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    let date = new Date(day)
+    return days[date.getDay()]
 }
-console.log(calNum(9, "/", 0));
+console.log(getDayName("3/26/2024"))
+
+//NOTE - ES6
+const getDayNameTwo = day => new Date(day).toLocaleDateString('en-us', {weekday: 'long'})
+console.log(getDayNameTwo("2024-03-26"))
